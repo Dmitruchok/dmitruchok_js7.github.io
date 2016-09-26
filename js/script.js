842be93f2300572f89701af56f47821de2d8ed51
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-27 01:01:50 +0300

    7

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOMContentLoaded');
+});
+
+$(function () {
+  var $oneList = $('.one');
+  var $tab_1 = $('.tab_1')
+
+  $oneList.on('click', function () {
+    $(this).css({
+      background: 'blue'
+    })
+    $tab_1.css({
+      display: 'block'
+    })
+  })
+});
