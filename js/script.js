3d19263c6aaa237b25f1e0a5b63ab0a6d3506309
Andrei Dmytruk <mallboroks@mail.ru>
2016-09-27 13:53:27 +0300

    7

--- js/script.js
+++ js/script.js
@@ -14,85 +14,38 @@ $(function () {
   $wrapper = $('.wrapper');
 
   $oneList.on('click', function () {
-    $(this).css({
-      background: '#007fff'
-    });
-    $tab_1.css({
-      display: 'block'
-    });
-    $wrapper.css({
-      overflow: 'none'
-    });
-    $container.css({
-      margin: '10px 0px 10px 10px'
-    });
-    $twoList.css({
-      background: '#fff'
-    });
-    $threeList.css({
-      background: '#fff'
-    });
-    $tab_2.css({
-      display: 'none'
-    });
-    $tab_3.css({
-      display: 'none'
-    });
+    $(this).css( 'background', '#007fff' ),
+    $tab_1.css( 'display', 'block'),
+    $wrapper.css( 'overflow', 'none' );
+    $container.css( 'margin', '10px 0px 10px 10px' ),
+    $twoList.css( 'background', '#fff' ),
+    $threeList.css( 'background', '#fff' ),
+    $tab_2.css( 'display', 'none' ),
+    $tab_3.css( 'display', 'none');
   });
 
   $twoList.on('click', function () {
-    $(this).css({
-      background: '#007fff'
-    });
-    $tab_2.css({
-      display: 'block'
-    });
-    $wrapper.css({
-      overflow: 'none'
-    });
-    $container.css({
-      margin: '10px 0px 10px 10px'
-    });
-    $oneList.css({
-      background: '#fff'
-    });
-    $threeList.css({
-      background: '#fff'
-    });
-    $tab_1.css({
-      display: 'none'
-    });
-    $tab_3.css({
-      display: 'none'
-    });
+    $(this).css( 'background', '#007fff' ),
+    $tab_2.css( 'display', 'block' ),
+    $wrapper.css( 'overflow', 'none' ),
+    $container.css( 'margin', '10px 0px 10px 10px' ),
+    $oneList.css( 'background', '#fff' ),
+    $threeList.css( 'background', '#fff' ),
+    $tab_1.css( 'display', 'none' ),
+    $tab_3.css( 'display', 'none' );
   });
 
   $threeList.on('click', function () {
-    $(this).css({
-      background: '#007fff'
-    });
+    $(this).css( 'background', '#007fff');
     $tab_3.css({
       display: 'block',
       height: 'auto'
-    });
-    $wrapper.css({
-      overflow: 'auto'
-    });
-    $container.css({
-      margin: '10px 0 10px 10px'
-    });
-    $oneList.css({
-      background: '#fff'
-    });
-    $twoList.css({
-      background: '#fff'
-    });
-
-    $tab_1.css({
-      display: 'none'
-    });
-    $tab_2.css({
-      display: 'none'
-    });
+    }),
+    $wrapper.css( 'overflow', 'auto' ),
+    $container.css( 'margin', '10px 0 10px 10px' ),
+    $oneList.css( 'background', '#fff' ),
+    $twoList.css( 'background', '#fff'),
+    $tab_1.css( 'display', 'none' ),
+    $tab_2.css( 'display', 'none' );
   });
 });
